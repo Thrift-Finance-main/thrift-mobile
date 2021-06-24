e73887b3805963a26cb5f4fc147576459b5f3549
Nicholas Samaras <n.samaras4@outlook.com>
2021-06-24 17:29:39 -0400

    Framework Files

---
+++ thrift-finance/src/Application.js
@@ -0,0 +1,16 @@
+import React, {Component} from 'react';
+
+class Application extends Component {
+
+    render(){
+        
+        return (
+            <div>
+            <h1>Thrift-Finance</h1>
+            <span>Basic framework being established</span>
+            </div>
+        )
+    }
+}
+
+export default Application;
