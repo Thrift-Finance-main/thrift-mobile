26d4a22decb64e43b187c5ea485ed19fe6cc4321
jcaso <jcaso@protonmail.com>
2021-09-22 10:40:43 +0100

    fix: remove unused code from AppWrapper

--- src/AppWrapper.tsx
+++ src/AppWrapper.tsx
@@ -1,22 +1,10 @@
 import React from 'react';
 import {NavigationContainer} from "@react-navigation/native";
-import { createBottomTabNavigator } from '@react-navigation/bottom-tabs';
 // @ts-ignore
 import Ionicons from 'react-native-vector-icons/Ionicons';
-import {useTranslation} from "react-i18next";
 import {MainStackNavigator} from "./navigation/MainStackNavigator";
 
-const Tab = createBottomTabNavigator();
-
 function AppWrapper() {
-    // const { t, i18n } = useTranslation();
-    const { t, i18n, ready } = useTranslation();
-    const homeName = t('navBottomTabs.home');
-    const settingsName = t('navBottomTabs.settings');
-    const movementsName = t('navBottomTabs.movements');
-    const analyticsName = t('navBottomTabs.analytics');
-    const servicesName = t('navBottomTabs.services');
-
     return (
         <NavigationContainer>
            <MainStackNavigator/>
