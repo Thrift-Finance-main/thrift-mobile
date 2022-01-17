585695be7dc7ad3d9c921ae142c1444d8ba20531
jcaso <jcaso@protonmail.com>
2022-01-17 21:11:20 +0000

    feat: prepare for front integration

--- src/AppWrapper.tsx
+++ src/AppWrapper.tsx
@@ -1,15 +1,18 @@
 import React from 'react';
-import {NavigationContainer} from "@react-navigation/native";
+import {NavigationContainer} from '@react-navigation/native';
 // @ts-ignore
+// eslint-disable-next-line @typescript-eslint/no-unused-vars
 import Ionicons from 'react-native-vector-icons/Ionicons';
-import {MainStackNavigator} from "./navigation/MainStackNavigator";
+// eslint-disable-next-line import/extensions,import/no-unresolved
+import {MainStackNavigator} from './navigation/MainStackNavigator';
 
+// TODO: replace here with the frontend
 function AppWrapper() {
-    return (
-        <NavigationContainer>
-           <MainStackNavigator/>
-        </NavigationContainer>
-    );
+  return (
+    <NavigationContainer>
+      <MainStackNavigator />
+    </NavigationContainer>
+  );
 }
 
 export default AppWrapper;
