44db1c50480188f6ce1d03e17ddd3bc32cba5dfe
jcaso <jcaso@protonmail.com>
2022-03-15 23:10:06 +0000

    feat: add types

--- src/api/Blockfrost.ts
+++ src/api/Blockfrost.ts
@@ -1,5 +1,8 @@
-// eslint-disable-next-line import/prefer-default-export
+// eslint-disable-next-line import/prefer-default-export,import/extensions,import/no-unresolved
+import {BLOCKFROST_API, BLOCKFROST_URL_TESTNET} from '../../config';
+
 export const Blockfrost = () => {
+  const token = BLOCKFROST_API;
   return {
     name: 'data',
     exec: {
@@ -12,7 +15,7 @@ export const Blockfrost = () => {
         const address = `/api/blockfrost${endpoint}`;
 
         const response = await fetch(address, {
-          headers: {'Content-Type': 'application/json'},
+          headers: {'Content-Type': 'application/json', project_id: token},
           method: type,
         })
           .then(r => r.json())
@@ -22,4 +25,33 @@ export const Blockfrost = () => {
       },
     },
   };
+};
+
+export const submitTxBlockfrost = async (endpoint: string, data: any) => {
+  console.log('submitTxBlockfrost');
+  const address = `/api/blockfrost${endpoint}`;
+  const token = BLOCKFROST_API;
+  const urlb = BLOCKFROST_URL_TESTNET + address;
+
+  console.log('token');
+  console.log(token);
+  const result = await fetch(urlb, {
+    headers: {
+      Authorization: `Bearer \${${token}}`,
+      project_id: token,
+      'Content-Type': 'application/cbor',
+      'User-Agent': 'thrift-wallet',
+      'Cache-Control': 'no-cache',
+    },
+    method: Buffer.from(data, 'hex') ? 'POST' : 'GET',
+    body: Buffer.from(data, 'hex'),
+  }).then(res => res.json());
+
+  if (result.status_code === 400) {
+    return {
+      error: result,
+    };
+  }
+
+  return result;
 };
--- src/db/model/AddressModel.ts
+++ src/db/model/AddressModel.ts
@@ -1,6 +1,7 @@
 export interface IAddress {
   _id?: string;
   reference: string;
+  tags: Set<string>;
   index: number;
   address: string;
   network: string;
@@ -12,6 +13,7 @@ export const AddressSchema = {
   properties: {
     _id: 'uuid',
     reference: 'string',
+    tags: 'string<>', // Set of tags
     index: 'int',
     address: 'string',
     network: 'string',
