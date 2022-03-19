942ea1a13b9b08339bb4b0f5f13a8377d5ce744d
jcaso <jcaso@protonmail.com>
2022-03-19 00:21:57 +0000

    feat: addd rnuli deps

--- App.tsx
+++ App.tsx
@@ -4,12 +4,28 @@ import {Provider} from 'react-redux';
 import thunk from 'redux-thunk';
 import Reducers from './src/store/Reducers';
 import Routes from './src/navigation/Routes';
+import configureStore from "./src/redux/reducers/configureStore";
+import realmDb from "./src/db/RealmConfig";
+import {setCurrentAccount} from "./src/redux/actions/AccountActions";
+
+
+
 
 const rootReducer = combineReducers({
   Reducers,
 });
-const store = createStore(rootReducer, applyMiddleware(thunk));
 
+const initApp = () => {
+    console.log('\n\n\ninitApp');
+    const store = createStore(rootReducer, applyMiddleware(thunk));
+    realmDb.getCurrentAccount().then(accountName => {
+        console.log('accountName in InitApp');
+        console.log(accountName);
+    });
+
+    return store;
+};
+const store = initApp();
 const App = () => {
   return (
     <Provider store={store}>
