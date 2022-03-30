64a52c28708a42b4291c139fd7ff16d1285d1545
jcaso <jcaso@protonmail.com>
2022-03-30 20:37:46 +0100

    fix: remove unused code

--- src/navigation/Routes.tsx
+++ src/navigation/Routes.tsx
@@ -1,101 +1,3 @@
-// import React from 'react';
-// import { View } from 'react-native';
-// import Colors from '../constants/CustomColors';
-// import { NavigationContainer } from '@react-navigation/native';
-// import { createNativeStackNavigator } from '@react-navigation/native-stack';
-// import { createBottomTabNavigator } from '@react-navigation/bottom-tabs';
-// import LanguageScreen from '../screens/LanguageScreen';
-// import AuthenticationScreen from '../screens/AuthenticationScreen';
-// import WelcomeScreen from '../screens/WelcomeScreen';
-// import CreateAccountScreen from '../screens/CreateAccountScreen';
-// import TermsScreen from '../screens/TermsScreen';
-// import CopyPhraseScreen from '../screens/CopyPhraseScreen';
-
-// import WalletScreen from '../screens/WalletScreen';
-// import SmartFiScreen from '../screens/SmartFiScreen';
-// import SettingsScreen from '../screens/SettingsScreen';
-// import VerifyPhraseScreen from '../screens/VerifyPhraseScreen';
-// import CreatePinScreen from '../screens/CreatePinScreen';
-// import RestoreWalletScreen from '../screens/RestoreWalletScreen';
-// import ShowQRCodeScreen from '../screens/ShowQRCodeScreen';
-// import BottomTabBar from './BottomTabBar';
-// import SecurityScreen from '../screens/SecurityScreen';
-// import ChangePasswordScreen from '../screens/ChangePasswordScreen';
-
-// const AuthenticationStack = createNativeStackNavigator();
-// const MainStack = createNativeStackNavigator();
-
-// const Tab = createBottomTabNavigator();
-
-// const DashboardTab = () => {
-//     return (
-//         <View
-//             style={{
-//                 flex: 1,
-//                 backgroundColor: Colors.background,
-//             }}>
-//             <Tab.Navigator
-//                 screenOptions={{
-//                     headerShown: false,
-//                     tabBarShowLabel: false,
-
-//                     // tabBarStyle: {backgroundColor: 'red', marginBottom: 20},
-//                 }}
-//                 tabBar={props => <BottomTabBar {...props} />}>
-//                 <Tab.Screen name="Wallet" component={WalletScreen}
-
-//                 />
-//                 <Tab.Screen name="SmartFi" component={SmartFiScreen} />
-//                 <Tab.Screen name="Settings" component={SettingsScreen}
-//                   options={{tabBarStyle:{display:"none"}}}
-//                 />
-
-//             </Tab.Navigator>
-//         </View>
-//     );
-// };
-// const MainStackNavigation = () => {
-//     return (
-//         <MainStack.Navigator screenOptions={{ headerShown: false }}>
-
-//             <MainStack.Screen name="DashboardTab" component={DashboardTab} />
-//             <MainStack.Screen name="Security" component={SecurityScreen} />
-//             <MainStack.Screen name="ChangePassword" component={ChangePasswordScreen} />
-
-//         </MainStack.Navigator>
-//     );
-// };
-// const AuthenticationStackNavigation = () => {
-//     return (
-//         <AuthenticationStack.Navigator screenOptions={{ headerShown: false }}>
-//             <AuthenticationStack.Screen name="Language" component={LanguageScreen} />
-//             <AuthenticationStack.Screen name="Authentication" component={AuthenticationScreen} />
-//             <AuthenticationStack.Screen name="Welcome" component={WelcomeScreen} />
-//             <AuthenticationStack.Screen name="CreateAccount" component={CreateAccountScreen} />
-//             <AuthenticationStack.Screen name="Terms" component={TermsScreen} />
-//             <AuthenticationStack.Screen name="CopyPhrase" component={CopyPhraseScreen} />
-//             <AuthenticationStack.Screen name="VerifyPhrase" component={VerifyPhraseScreen} />
-//             <AuthenticationStack.Screen name="CreatePin" component={CreatePinScreen} />
-//             <AuthenticationStack.Screen name="RestoreWallet" component={RestoreWalletScreen} />
-//             <AuthenticationStack.Screen name="ShowQRCode" component={ShowQRCodeScreen} />
-
-//         </AuthenticationStack.Navigator>
-//     );
-// };
-
-// const Routes = () => {
-
-//     return (
-
-//         <NavigationContainer>
-//             <MainStackNavigation />
-//         </NavigationContainer>
-
-//     );
-// };
-
-// export default Routes;
-
 import React from 'react';
 import {View} from 'react-native';
 import {NavigationContainer} from '@react-navigation/native';
@@ -126,7 +28,6 @@ import CreateTargetScreen from '../screens/CreateTargetScreen';
 import SavingsScreen from '../screens/SavingsScreen';
 
 const AuthenticationStack = createNativeStackNavigator();
-const MainStack = createNativeStackNavigator();
 
 const Tab = createBottomTabNavigator();
 
@@ -142,9 +43,7 @@ const DashboardTab = () => {
       <Tab.Navigator
         screenOptions={{
           headerShown: false,
-          tabBarShowLabel: false,
-
-          // tabBarStyle: {backgroundColor: 'red', marginBottom: 20},
+          tabBarShowLabel: false
         }}
         tabBar={props => <BottomTabBar {...props} />}>
         <Tab.Screen name="Wallet" component={WalletScreen} />
@@ -158,17 +57,7 @@ const DashboardTab = () => {
     </View>
   );
 };
-// const MainStackNavigation = () => {
-//     return (
-//         <MainStack.Navigator screenOptions={{ headerShown: false }}>
 
-//             <MainStack.Screen name="DashboardTab" component={DashboardTab} />
-//             <MainStack.Screen name="Security" component={SecurityScreen} />
-//             <MainStack.Screen name="ChangePassword" component={ChangePasswordScreen} />
-
-//         </MainStack.Navigator>
-//     );
-// };
 const AuthenticationStackNavigation = () => {
   return (
     <AuthenticationStack.Navigator screenOptions={{headerShown: false}}>
